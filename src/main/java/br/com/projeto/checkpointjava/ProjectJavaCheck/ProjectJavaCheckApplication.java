1ed14d9ed25c1904992db69c89574a52c9b14329
Matteus VIegas <msmatteusbr2@gmail.com>
2025-11-10 19:24:54 -0300

    Sprint inicial

---
+++ src/main/java/br/com/projeto/checkpointjava/ProjectJavaCheck/ProjectJavaCheckApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.checkpointjava.ProjectJavaCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJavaCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJavaCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/checkpointjava/ProjectJavaCheck/ProjectJavaCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.checkpointjava.ProjectJavaCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJavaCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
